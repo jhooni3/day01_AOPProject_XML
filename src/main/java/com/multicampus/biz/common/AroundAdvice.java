e353d2fa61a01b9ab2bc5561be9f5e965a2571ac
muzi.k <jhoonik@gmail.com>
2019-08-06 23:33:26 +0900

    2일차 커밋

---
+++ src/main/java/com/multicampus/biz/common/AroundAdvice.java
@@ -0,0 +1,22 @@
+package com.multicampus.biz.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+public class AroundAdvice {
+	public Object aroundLog(ProceedingJoinPoint jp) throws Throwable {
+		String method = jp.getSignature().getName();
+		Object obj = null;
+		long start = System.currentTimeMillis();
+		
+		// 클라이언트가 호출한 비즈니스 메소드가 여기에서 실행됨
+		obj = jp.proceed();
+		
+		long end = System.currentTimeMillis();
+		System.out.println(method +"() 메소드 수행에 소요된 시간 : " + (end - start) + "(ms)초");
+		return obj;
+	}
+}
---
+++ src/main/java/com/multicampus/biz/common/LogAdvice.java
@@ -0,0 +1,16 @@
+package com.multicampus.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+public class LogAdvice {
+	public void printLog(JoinPoint jp) {
+		String method = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+		
+		System.out.println("[사전 처리] " + method + 
+			"() 메소드 ARGS 정보 : " + args[0].toString());
+	}
+}
